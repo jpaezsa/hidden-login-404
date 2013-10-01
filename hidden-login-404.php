e03bc4bbe5d9911f438b4fefc225e07c7b57d3a8
vovasik <vovasik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-01 07:31:44 +0000

    Stabile
    git-svn-id: https://plugins.svn.wordpress.org/hidden-login-404/trunk@780779 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hidden-login-404.php
@@ -0,0 +1,108 @@
+<?php
+/*
+Plugin Name: Hidden Login 404
+Description: Hides the login page
+Version: 1.0
+Author: Petrozavodsky
+Author URI: http://alkoweb.ru/
+Plugin URI: http://alkoweb.ru/hidden-login
+*/
+	define('hidden_login_404_DIR', plugin_dir_path(__FILE__));
+	define('hidden_login_404_URL', plugin_dir_url(__FILE__));
+	require_once( ABSPATH . "wp-includes/pluggable.php" );
+
+	register_activation_hook(__FILE__, 'hidden_login_404_activation');
+	register_deactivation_hook(__FILE__, 'hidden_login_404_deactivation');
+
+	function functions_add_hidde_add($rules ){
+		if (!is_plugin_active(hidden_login_404_DIR . 'hidden-login.php')) {
+			$rules="\r\n<IfModule mod_rewrite.c> \r\nRewriteEngine On \r\nRewriteRule ^su.php$ wp-login.php [L] \r\n</IfModule>\r\n\r\n".$rules ;
+		}	
+	return $rules;	
+	}
+
+	function functions_add_hidde_remove($rules ){
+	return $rules;	
+	}
+
+	function hidden_login_404_activation($rules) {
+		global $wp_rewrite;
+		add_filter( 'mod_rewrite_rules', 'functions_add_hidde_add' );
+		$wp_rewrite->flush_rules();
+	}
+	 
+ 
+	function hidden_login_404_deactivation() {
+		global $wp_rewrite;
+		add_filter( 'mod_rewrite_rules', 'functions_add_hidde_remove' );
+		$wp_rewrite->flush_rules();
+	}
+
+	function login_page_jquery_scripts() {
+		wp_enqueue_script( 'jquery' );
+	}    
+	add_action( 'login_enqueue_scripts', 'login_page_jquery_scripts' );
+	
+		function hide_login_add_js(){	
+			$request_url_login = basename( $_SERVER['REQUEST_URI']);
+			if($request_url_login == 'su.php' || $_GET['action'] == 'rp' ){
+				echo '<script>jQuery(document).ready(function($) { 
+				$("#nav a").attr("href","su.php?action=lostpassword"); 
+				$("#loginform").attr("action","su.php"); 
+				});</script>';
+			}	
+
+			if($request_url_login == 'su.php?action=lostpassword' ){
+				echo '<script>jQuery(document).ready(function($) { 
+				$("#nav a").attr("href","su.php"); 
+				$("#lostpasswordform").attr("action","su.php?action=lostpassword"); 
+				});</script>';
+			}
+			if( $_GET['action'] == 'resetpass'){
+				echo '<script>jQuery(document).ready(function($) { 
+				$(".reset-pass a").attr("href","su.php"); 
+				});</script>';
+			}
+		}	
+		add_action( 'login_head', 'hide_login_add_js');
+		
+		if(basename( $_SERVER['REQUEST_URI']) == 'wp-login.php?checkemail=confirm'){
+			function hide_login_checkemail(){
+				wp_redirect( 'su.php?checkemail=confirm', 301 );   
+			}
+		add_action( 'login_init', 'hide_login_checkemail');	
+		}			
+		
+		if( $_GET['action'] !== 'rp'){
+			if( $_GET['action'] !== 'resetpass'){
+				$book_collection[] = 'su.php?action=lostpassword';
+				$book_collection[] = 'su.php?checkemail=confirm';
+				$book_collection[] = 'wp-login.php?checkemail=confirm';
+				$book_collection[] = 'su.php';
+				$book_collection = array_flip($book_collection);
+				$book = basename( $_SERVER['REQUEST_URI']);
+				
+				if ( !isset($book_collection[$book]) ) {
+					function hide_login(){
+						wp_redirect( '404.php', 301 );   
+					}
+					add_action( 'login_init', 'hide_login');	
+				}		
+			}
+		}		
+
+	if( basename( $_SERVER['REQUEST_URI']) == 'su.php' ){
+		if ( is_user_logged_in() ) {
+			wp_redirect( 'wp-admin', 301 );   
+		}
+	}
+
+	function custom_loginout_default_page($logout_url, $redirect) {
+	  $go_to_url = site_url( 'su.php' );
+	  if ( empty($redirect) )
+		$logout_url = add_query_arg('redirect_to', urlencode($go_to_url), $logout_url);
+	  return $logout_url;
+	}
+	add_filter('logout_url', 'custom_loginout_default_page', 10, 2);
+
+?>
---
+++ readme.txt
@@ -0,0 +1,67 @@
+=== Hidden Login 404 ===
+Contributors: (petrozavodsky, vovasik)
+Donate link: http://alkoweb.ru/
+Tags: login form, brute force, spam
+Requires at least: Your server is running PHP version 5.2 but WordPress 3.0.1 
+Tested up to: 3.6
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Plug-in to hide the login page, change the url: wp-login.php and wp-admin maintaining functionality
+
+== Description ==
+
+In connection with the cases of attempts to guess passwords to the administrative panel on the CMS website, the need to plug protects your authorization form.
+Plugin Hidden Login 404 redirects requests to the administration of the site at an arbitrary URL is hampering password guessing attempts. 
+404 received a response from the server, most of the brute Forz Bots stop guessing attempts without creating a heavy load on the blog.
+Attention.
+After activating the plugin, you will not be able to log in to wordpress, standard obrazaom pages wp-login.php and wp-admin.
+
+== Installation ==
+1. Upload `hidden_login` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= After activating the plugin can not login. What to do =
+
+After activating the plugin, your admin panel will be on yadostupna url http://youdomine.com/su.php
+
+
+= Remove and uninstall the plugin manually =
+
+1. Delete the directory 'hidden_login' from the directory 'plugins'
+2. Remove from. Htaccess file lines:
+
+<IfModule mod_rewrite.c> 
+RewriteEngine On 
+RewriteRule ^su.php$ wp-login.php [L] 
+</IfModule>
+
+== Screenshots ==
+
+1. Activating the plugin.
+
+== Changelog ==
+
+= 1.0 =
+
+Added redirect authorized user
+
+== Upgrade Notice ==
+
+= 1.0 =
+
+This version has become possible to apply directly to the url http:// youdomine.com/su.php with the book, with the fall in the admin panel
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the [GNU General Public License][] as published by the Free
+Software Foundation; either version 2 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+[GNU General Public License]: http://www.gnu.org/copyleft/gpl.html
